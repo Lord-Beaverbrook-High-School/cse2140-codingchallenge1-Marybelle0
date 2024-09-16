e07b8e86d3c6aaa24979d760c98dcef9070a677b
38089504 <marybelleo1@educbe.ca>
2024-09-16 09:50:12 -0600

    finished

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,27 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner myScan = new Scanner(System.in);
-        System.out.println("Emter a 4 digit year")
+        System.out.println("Enter a 4 digit year");
         int year = myScan.nextInt();
         /* using MATHEMATICAL operations (no Strings), split a year
         Example: if the user enters 1989, the output would be:
         1
         9
         8
-        9
+        9 */
+        // retrieves rightmost digit, assigns it to a variable (digX), then removes it from year
+        int digOne = year % 10;
+        year = (year - digOne) / 10;
+        int digTwo = year % 10;
+        year = (year - digTwo) / 10;
+        int digThree = year % 10;
+        year = (year - digThree) / 10;
+        int digFour = year;
+        //prints each digit on seperate lines, starting from the leftmost digit of the input
+        System.out.println(digFour);
+        System.out.println(digThree);
+        System.out.println(digTwo);
+        System.out.println(digOne);
 
-         */
-    }
 }
-//Comment on how you tested the code to make sure it works in all situations
-//include tests that you ran
+}
